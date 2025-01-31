9297827526b6d332d2762668bc4d3116528beb6d
Tingwei_Liu <tingwel4@uci.edu>
2025-01-31 01:39:49 -0800

    m

--- get_Sim_distribution.py
+++ get_Sim_distribution.py
@@ -11,7 +11,7 @@ def get_PPE(sim_file,trigger_name,output_path):
 '''
 trig_energy, trig_zenith, trig_azimuth, trig_weight, events_id=get_PPE(simulation_files_folder, trigger_name, e_bins=None, zen_bins=None, rate_per_bin=None, n_trig_per_bin=None):
 '''
-raw_sim = '/dfs8/sbarwick_lab/ariannaproject/rricesmi/simStn51/1.23.25/'
+raw_sim = '/dfs8/sbarwick_lab/ariannaproject/rricesmi/simStn51/1.23.25/Stn51_IceTop_18.4-18.5eV_0.*'
 test_raw= '/Users/david/PycharmProjects/Demo1/test_files'
 # output  = '/pub/tingwel4/station_51/sim_with_weights'
 output='/Users/david/PycharmProjects/Demo1/test_files'
@@ -19,6 +19,7 @@ thresh23_3      ='direct_LPDA_2of3_3.5sigma'
 for a in os.listdir(test_raw):
     if a.endswith('.nur'):
         print(a)
+
 # trig_energy, trig_zenith, trig_azimuth, trig_weight = get_PPE(sim_folder, trigger_names[0], e_range, sin2Val, rate_per_bin, n_trig_per_bin)
 if not os.path.isdir(output):
     os.makedirs(output)
