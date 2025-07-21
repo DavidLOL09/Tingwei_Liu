fc62f103be78ab3ef738b3c0a95ec408eeade20a
Tingwei_liu <tingwel4@uci.edu>
2025-07-20 22:43:34 -0700

    Simulation

--- Simulation/Stn51BatchJob.py
+++ Simulation/Stn51BatchJob.py
@@ -5,10 +5,10 @@ import os
 import shutil
 import send2trash
 
-n_cores     =   10    #10 Temp    #1000 Sim
+n_cores     =   200    #10 Temp    #1000 Sim
 num_icetop  =   30      #10 Temp    #30   Sim  
 amp = True
-add_noise = False
+add_noise = True
 output_folder = '/pub/tingwel4/output/CR_BL_Simulation/'
 output_filename = 'Stn51_IceTop'
 
