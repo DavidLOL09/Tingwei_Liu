0a8cb6b7b5ff0a95ddedd965119cb2bfe4c93576
Tingwei_liu <tingwel4@uci.edu>
2025-08-03 18:32:59 -0700

    weight45

--- Simulation/A00_SlurmUtil.py
+++ Simulation/A00_SlurmUtil.py
@@ -28,7 +28,7 @@ def makeAndRunJob(commandToRun, jobName='job', partition='free', runDirectory='r
     header += "#SBATCH --mem-per-cpu=18G		            ##18GB memory per job\n"
     header += "#SBATCH --output={}\n".format(os.path.join(runDirectory, 'logs', f'{jobName}.out'))
     header += "#SBATCH --error={}\n".format(os.path.join(runDirectory, 'logs', f'{jobName}.err'))
-    if 18.5 in cmd:
+    if '18.5' in cmd:
         header += "#SBATCH --mail-type=fail,end,begin\n"
         # header += "#SBATCH --mail-type=end\n"
     header += "#SBATCH --mail-user=tingwel4@uci.edu\n"
