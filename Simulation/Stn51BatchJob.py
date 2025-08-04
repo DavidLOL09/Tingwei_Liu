2ff8105dcb12d48181004f0ddfa91c957da24633
Tingwei_liu <tingwel4@uci.edu>
2025-08-03 17:57:16 -0700

    weight37

--- Simulation/Stn51BatchJob.py
+++ Simulation/Stn51BatchJob.py
@@ -36,6 +36,6 @@ for e in e_range:
     for sin2 in sin2Val:
         # e = 18.4
         # sin2 = 0.0
-        cmd = f'python Stn51Simulation.py {output_folder}{output_filename}_{e:.1f}-{e+0.1:.1f}eV_{sin2:.1f}sin2_{n_cores}cores* {n_cores} --min_energy {e:.1f} --max_energy {e+0.1:.1f} --sin2 {sin2:.1f} --num_icetop {num_icetop}'
+        cmd = f'python Stn51Simulation.py {output_folder}{output_filename}_{e:.1f}-{e+0.1:.1f}eV_{sin2:.1f}sin2_{n_cores}cores {n_cores} --min_energy {e:.1f} --max_energy {e+0.1:.1f} --sin2 {sin2:.1f} --num_icetop {num_icetop}'
         A00_SlurmUtil.makeAndRunJob(cmd, f'Stn51_{e:.1f}_{sin2:.1f}sin2', runDirectory='run_RateCalc', partition='standard')
         # quit()
