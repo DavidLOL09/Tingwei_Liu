c66615824ab0b20cfe2e57520332fe407381c3fa
Tingwei_liu <tingwel4@uci.edu>
2025-08-03 17:47:54 -0700

    weight35

--- Simulation/Stn51RateCalc.py
+++ Simulation/Stn51RateCalc.py
@@ -122,9 +122,9 @@ def getParametersPerEvent(simulation_files_folder, trigger, output, filename, ma
 
     nurFiles= []
 
-    for file in os.listdir(sim_folder):
+    for file in os.listdir(simulation_files_folder):
         if file.endswith('.nur') and (f'{e_bins[0]:.1f}-{e_bins[1]:.1f}eV_{zen_bins[0]:.1f}sin2' in file):
-            nurFiles.append(os.path.join(sim_folder, file))
+            nurFiles.append(os.path.join(simulation_files_folder, file))
     if nurFiles == []:
         return [],[],[],[],[]
     
