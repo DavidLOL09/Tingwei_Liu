c0aaa753ecd4db925da115b9e9faa9b06a130b9e
Tingwei_liu <tingwel4@uci.edu>
2025-08-14 12:23:19 -0700

    NuRadio

--- NuRadioMC_custmized/install_dev.py
+++
@@ -1,6 +0,0 @@
-print(
-    "install_dev.py has been deprecated. "
-    "To install NuRadioMC as a developer, please use\n\n"
-    "\tpip install -e .[dev]\n\n"
-    "instead to install in editable mode."
-)
