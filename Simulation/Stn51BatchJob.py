918275fe2c13299a90edfacbe69098086413cd8e
Tingwei_liu <tingwel4@uci.edu>
2025-08-15 09:37:48 -0700

    Time_check

--- Simulation/Stn51BatchJob.py
+++ Simulation/Stn51BatchJob.py
@@ -8,13 +8,13 @@ import time
 from icecream import ic
 import inspect
 
-n_cores     =   20    #10 Temp    #1000 Sim
+n_cores     =   100    #10 Temp    #1000 Sim
 num_icetop  =   10      #10 Temp    #30   Sim  
 amp = True
 add_noise = True
 # output_folder = '/pub/tingwel4/output/CR_BL_Simulation_demo/'
-output_origin = '/pub/tingwel4/output/CR_BL_Sim_Temp/origin_Time_check'
-output_backlope='/pub/tingwel4/output/CR_BL_Sim_Temp/backlope_Time_check'
+output_origin = '/pub/tingwel4/output/CR_BL_Sim_Temp/origin'
+output_backlope='/pub/tingwel4/output/CR_BL_Sim_Temp/backlope'
 output_filename = 'Stn51_IceTop'
 
 # Make directory if it doesn't exist
@@ -44,8 +44,8 @@ for e in e_range:
     for sin2 in sin2Val:
         # e = 18.4
         # sin2 = 0.0
-        ic(f'line {inspect.currentframe().f_lineno} in {os.path.basename(__file__)}:{time.perf_counter()}')
+        # ic(f'line {inspect.currentframe().f_lineno} in {os.path.basename(__file__)}:{time.perf_counter()}')
         cmd = f'python Stn51Sim_origin_backlope.py --output_filename {output_filename}_{e:.1f}-{e+0.1:.1f}eV_{sin2:.1f}sin2_{n_cores}cores {n_cores} --min_energy {e:.1f} --max_energy {e+0.1:.1f} --sin2 {sin2:.1f} --num_icetop {num_icetop} --output_origin {output_origin} --output_backlope {output_backlope}'
         A00_SlurmUtil.makeAndRunJob(cmd, f'Stn51_{e:.1f}_{sin2:.1f}sin2', runDirectory='run_backlope_time_check', partition='standard')
-        ic(f'line {inspect.currentframe().f_lineno} in {os.path.basename(__file__)}:{time.perf_counter()}')
-        quit()
+        # ic(f'line {inspect.currentframe().f_lineno} in {os.path.basename(__file__)}:{time.perf_counter()}')
+        # quit()
