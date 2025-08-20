2c614d0d83b9c1bbdf9b6e7a6fa1e07cffbfd6a4
Tingwei_liu <tingwel4@uci.edu>
2025-08-20 16:40:08 -0700

    backlope35

--- Simulation/A00_SlurmUtil.py
+++ Simulation/A00_SlurmUtil.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 
-def makeAndRunJob(commandToRun, jobName='job', partition='free', runDirectory='run/', n_cpus=10):
+def makeAndRunJob(commandToRun, jobName='job', partition='free', runDirectory='run/', n_cpus=1):
 
     print(f'Running job {jobName} with error files {runDirectory}/logs/{jobName}.out')
     os.makedirs(runDirectory, exist_ok=True)
@@ -25,7 +25,7 @@ def makeAndRunJob(commandToRun, jobName='job', partition='free', runDirectory='r
     header += "#SBATCH --nodes=1                        ##Nodes to be used\n"
     header += "#SBATCH --ntasks=1                       ##Numer of processes to be launched\n"
     header += f"#SBATCH --cpus-per-task={n_cpus}                ##Cpu's to be used\n"
-    header += "#SBATCH --mem-per-cpu=18G		            ##18GB memory per job\n"
+    header += "#SBATCH --mem-per-cpu=6G		            ##18GB memory per job\n"
     header += "#SBATCH --output={}\n".format(os.path.join(runDirectory, 'logs', f'{jobName}.out'))
     header += "#SBATCH --error={}\n".format(os.path.join(runDirectory, 'logs', f'{jobName}.err'))
     if '18.5' in cmd:
