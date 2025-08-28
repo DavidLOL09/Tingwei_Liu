e7e6a86b7acb3add1b5492250cb5c8baa7f336f9
Tingwei_liu <tingwel4@uci.edu>
2025-08-28 16:09:22 -0700

    phase fix 12

--- Simulation/Stn51BatchJob_f_b_seperation.py
+++ Simulation/Stn51BatchJob_f_b_seperation.py
@@ -32,7 +32,7 @@ max_energy = 17.6
 
 e_range = np.arange(min_energy, max_energy, 0.1)
 sin2Val = np.arange(0, 1.01, 0.1)
-sin2Val = np.arange([0.5,0.6,0.1])
+sin2Val = np.arange(0.5,0.6,0.1)
 
 
 # try:
