109f472e64ffd75095504c8aab41cdb636a68e43
Tingwei_liu <tingwel4@uci.edu>
2025-10-29 15:14:03 -0700

    RateCalc

--- Simulation/Batch_RateCalc.py
+++ Simulation/Batch_RateCalc.py
@@ -11,11 +11,11 @@ from icecream import ic
 n_cores     =   200    #10 Temp    #1000 Sim
 num_icetop  =   10     #10 Temp    #30   Sim  
 
-working_folder = '/pub/tingwel4/output/CR_BL_Simulation/'
+working_folder = '/pub/tingwel4/output/CR_BL_4_backlope_Simulation/'
 working_filename = 'Stn51_IceTop'
 batch_path='/pub/tingwel4/Tingwei_Liu/Simulation/evtRateCalc'
 run_py_file='get_Sim_distribution'
-output = '/pub/tingwel4/output/CR_BL_Simulation_weighted'
+output = '/pub/tingwel4/output/CR_BL_4_backlope_Simulation_weighted'
 
 
 # Make directory if it doesn't exist
